75747b8fa565251a5c591e9a352b119fe0d1de6b
TrevorS2001 <96450353+TrevorS2001@users.noreply.github.com>
2022-04-03 16:35:26 -0400

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly Used data types DO NOT include:",
+        choices: ["stings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statment is enclosed within _____.",
+        choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "String values must be enclosed within ________ when being asssigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+    {
+        title: "Arrays in a JavaScript can be used to store _______.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    }
+]
